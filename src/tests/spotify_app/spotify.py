55f353c6574387f3f36a2eb299f564d73a5d37a7
Erick Bryan Cubas <thecubas@outlook.com>
2024-09-17 01:29:46 -0300

    :test_tube: (caching and multipages) Decorating the data reading function

--- src/tests/spotify_app/spotify.py
+++ src/tests/spotify_app/spotify.py
@@ -1,6 +1,7 @@
 """_summary_
 """
 import os
+import time
 import streamlit as st
 import pandas as pd
 
@@ -10,27 +11,37 @@ st.set_page_config(
 )
 
 st.title('Análise de Dados Spotify')
+
 base_dir = os.path.abspath(os.path.join(os.path.dirname(__file__), '..', '..', '..'))
 file_path = os.path.join(base_dir, 'src', 'data', 'raw', 'spotify.csv')
 
 #st.write(f"Caminho absoluto do arquivo: {file_path}")
 
 if os.path.exists(file_path):
-    df = pd.read_csv(file_path)
 
+    @st.cache_data
+    def load_data():
+        """_summary_
+        Método para carregar os dados do arquivo csv
+        """
+        df = pd.read_csv(file_path)
+        time.sleep(3)
+        # Para operações pesadas
+        return df
+    df_spotify = load_data()
     # Persistindo os dados na sessão do browser
-    st.session_state['df_spotify'] = df
-    df.set_index("Track", inplace=True)
-    #st.write(df)
+    st.session_state['df_spotify'] = df_spotify
+    df_spotify.set_index("Track", inplace=True)
+    #st.write(df_spotify)
 
     # Controlador de seleção dos artista
-    artists = df['Artist'].value_counts().index
+    artists = df_spotify['Artist'].value_counts().index
     artist = st.sidebar.selectbox("Artista", artists)
 
     # Controlador de seleção dos albuns
-    albums = df[df['Artist'] == artist]['Album'].value_counts().index
+    albums = df_spotify[df_spotify['Artist'] == artist]['Album'].value_counts().index
     album = st.sidebar.selectbox('Album', albums)
-    df_filtered_album = df[df['Album'] == album]
+    df_filtered_album = df_spotify[df_spotify['Album'] == album]
 
     # Controlador de exibição dos dados
     #display = st.checkbox("Mostrar dados em gráfico")
